12554bbc63ff0140826949be59b228bb8200bdee
c0a24030 <c0a240300f@edu.teu.ac.jp>
2025-04-15 16:32:35 +0900

    練習10:とりうごいた

--- pygame_image.py
+++ pygame_image.py
@@ -13,15 +13,28 @@ def main():
     bg_img2 = pg.transform.flip(bg_img,True,False)
     Tori_1 = pg.image.load("fig/3.png")
     Tori_1 = pg.transform.flip(Tori_1,True,False)
+    Tori_rct = Tori_1.get_rect()
+    Tori_rct.center = 300, 200
+    key_lst = pg.key.get_pressed()
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            Tori_rct.move_ip((0,-1))
+        if key_lst[pg.K_DOWN]:
+            Tori_rct.move_ip((0,1))
+        if key_lst[pg.K_LEFT]:
+            Tori_rct.move_ip((-1,0))
+        if key_lst[pg.K_RIGHT]:
+            Tori_rct.move_ip((1,0))
+
         x = tmr%3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2,[-x+1600,0])
         screen.blit(bg_img,[-x+3200,0])
-        screen.blit(Tori_1,[300,200])
+        screen.blit(Tori_1,Tori_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
