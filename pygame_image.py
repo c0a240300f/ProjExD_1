f7b2a0d01337bbf61de8ab71bab2571e331fb644
c0a24030 <c0a240300f@edu.teu.ac.jp>
2025-04-15 15:52:54 +0900

    練習6:背景を左に動作

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img,True,False)
     Tori_1 = pg.image.load("fig/3.png")
     Tori_1 = pg.transform.flip(Tori_1,True,False)
     tmr = 0
@@ -17,7 +18,7 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [-tmr, 0])
         screen.blit(Tori_1,[300,200])
         pg.display.update()
         tmr += 1        
